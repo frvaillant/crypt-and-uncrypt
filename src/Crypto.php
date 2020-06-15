a754533f8bae1d65d5dbe0f1e467ce788164eb55
francoisvaillant <frvaillant@gmail.com>
2020-06-16 00:17:29 +0200

    started

---
+++ src/Crypto.php
@@ -0,0 +1,65 @@
+<?php
+namespace App;
+
+use App\CryptoMaker;
+
+/**
+ * Class Crypto
+ * @package App
+ */
+class Crypto
+{
+
+    /**
+     * @var
+     */
+    private $output;
+
+    /**
+     * @const
+     */
+    const PRIVATE_KEY = 'LaNuitEstSiJolieEnRoseEtBleu!Non?';
+
+
+    /**
+     * @param $input
+     * @return $this
+     */
+    public function hide($input)
+    {
+        $data = CryptoMaker::encrypt($input, self::PRIVATE_KEY);
+        $this->setOutput($data);
+        return $this;
+    }
+
+    /**
+     * @param $input
+     * @return $this
+     */
+    public function show($input)
+    {
+        $data = CryptoMaker::decrypt($input, self::PRIVATE_KEY);
+        $this->setOutput($data);
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getResult()
+    {
+        return $this->output;
+    }
+
+
+    /**
+     * @param mixed $output
+     */
+    public function setOutput($output)
+    {
+        $this->output = $output;
+    }
+
+
+
+}
